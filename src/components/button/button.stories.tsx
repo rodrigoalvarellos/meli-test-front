b1e25f73ca7fbc2d28918a12d9fc32264c5f1466
Rodrigo Alvarellos <rodri.alvarellos@gmail.com>
2021-04-18 18:30:44 -0300

    se agrego storybook

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { Button } from './button';
+ 
+
+export default {
+    title: "Buttton Primary",
+    component: Button,
+};
+
+export const buttonPrimary: React.FC = () => (
+    <Button>Comprar</Button>
+  );
+  
