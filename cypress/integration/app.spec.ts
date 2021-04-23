2cccc86fbc252ec6c017de8fb8c3d2ddf46e1649
Rodrigo Alvarellos <rodri.alvarellos@gmail.com>
2021-04-23 01:08:44 -0300

    .

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,59 +1,60 @@
 describe("App Page", () => {
-
-  beforeEach(() => {   
-
-  cy.intercept('http://localhost:3500/api/items?q=*',  {fixture: 'search.results.json' }).as('searchResults');
-  cy.intercept('http://localhost:3500/api/items?category=*',  {fixture: 'search.results.json' }).as('categorySearchResults');
-  cy.intercept('http://localhost:3500/api/items/*',  {fixture: 'product.detail.json' });
-  cy.visit('/');
-    
-  })
+  beforeEach(() => {
+    cy.intercept("http://localhost:3500/api/items?q=*", {
+      fixture: "search.results.json",
+    }).as("searchResults");
+    cy.intercept("http://localhost:3500/api/items?category=*", {
+      fixture: "search.results.json",
+    }).as("categorySearchResults");
+    cy.intercept("http://localhost:3500/api/items/*", {
+      fixture: "product.detail.json",
+    });
+    cy.visit("/");
+  });
 
   it("should search, find results, navigate to the firt one and show content correctly ", () => {
-       
-    cy.get('input').type('iphone');
-    cy.get('button').click();
+    cy.get("input").type("iphone");
+    cy.get("button").click();
 
-    cy.wait('@searchResults');
- 
-    cy.url().should('eq', 'http://localhost:3000/items?search=iphone');
+    cy.wait("@searchResults");
 
-    const listItemsFirstElement = cy.get(`[data-testid="SEARCH_LIST_TESTID"]>ul>li`).first();
+    cy.url().should("eq", "http://localhost:3000/items?search=iphone");
+
+    const listItemsFirstElement = cy
+      .get(`[data-testid="SEARCH_LIST_TESTID"]>ul>li`)
+      .first();
 
     listItemsFirstElement.click();
-    cy.url().should('eq', 'http://localhost:3000/items/MLA914734725');
+    cy.url().should("eq", "http://localhost:3000/items/MLA914734725");
 
-    const productDetail = cy.get(`[data-testid="PRODUCT_DETAIL_TESTID"]`);    
-    productDetail.should('contain.text', 'iPhone 11 64 Gb Negro');
+    const productDetail = cy.get(`[data-testid="PRODUCT_DETAIL_TESTID"]`);
+    productDetail.should("contain.text", "iPhone 11 64 Gb Negro");
   });
 
   it("should search by category when breadcrumb link is clicked ", () => {
+    cy.get("input").type("iphone");
+    cy.get("button").click();
 
-    cy.get('input').type('iphone');
-    cy.get('button').click();
+    cy.wait("@searchResults");
 
-    cy.wait('@searchResults');
-
-    const firstBreadcrumbElement = cy.get('[data-testid="BREADCRUMB_TESTID"]>ul>li>a').first();
+    const firstBreadcrumbElement = cy
+      .get('[data-testid="BREADCRUMB_TESTID"]>ul>li>a')
+      .first();
     firstBreadcrumbElement.click();
-    cy.url().should('eq', 'http://localhost:3000/items?category=MLA1051');
-    // cy.wait('@categorySearchResults');
-    
+    cy.url().should("eq", "http://localhost:3000/items?category=MLA1051");
   });
 
   it("should navigate to home when ML logo is clicked", () => {
+    cy.get("input").type("iphone");
+    cy.get("button").click();
 
-    cy.get('input').type('iphone');
-    cy.get('button').click();
+    cy.wait("@searchResults");
 
-    cy.wait('@searchResults');
- 
-    cy.url().should('eq', 'http://localhost:3000/items?search=iphone');
+    cy.url().should("eq", "http://localhost:3000/items?search=iphone");
 
     const logoML = cy.get(`[data-testid="SEARCHBOX_TESTID"]>*>a`);
     logoML.click();
 
-    cy.url().should('eq', 'http://localhost:3000/');
-      
+    cy.url().should("eq", "http://localhost:3000/");
   });
 });
