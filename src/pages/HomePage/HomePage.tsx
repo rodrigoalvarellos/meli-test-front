8c3b921fb7f592893995085f9d2ab80bf6b2c117
Rodrigo Alvarellos <rodri.alvarellos@gmail.com>
2021-04-25 21:41:23 -0300

    Agregando configuracion de codecov

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,32 @@
-import { FC } from 'react';
-// import styles from './HomePage.module.scss';
+import { FC, useEffect } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import texts from '../../config/text.config.json';
+// import styles from './HomePage.module.scss';
 
-interface IHomeProps extends RouteComponentProps {}
+interface IHomeProps extends RouteComponentProps {
+  setMetaTags: Function;
+}
 
 /**
  * This component represents the blank page when starting the application.
  */
-export const HomePage: FC<IHomeProps> = (props) => (
-  <>
-    {/* <div className={styles.HomePage__card_container}>
-             <Card >Hola</Card>
+export const HomePage: FC<IHomeProps> = ({ setMetaTags }) => {
+  const metaTexts = texts.header_meta_tags;
+
+  useEffect(() => {
+    setMetaTags({
+      title: `${metaTexts.title} - ${metaTexts.slogan}`,
+      description: metaTexts.description,
+      keywords: metaTexts.keys.join(',')
+    });
+  }, []);
+
+  return (
+    <>
+      {/* <div className={styles.HomePage__card_container}>
+             <Card ></Card>
 
         </div> */}
-  </>
-);
+    </>
+  );
+};
